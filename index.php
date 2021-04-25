03ab4b359ec7f0e97c01f3d0c8dd877eb633b740
job0629 <82936683+job0629@users.noreply.github.com>
2021-04-25 17:11:48 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+
+<html lang="">
+
+<head>
+  <title>Farm Production Auction</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="res/layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+
+<!-- head logo -->
+ <div class="wrapper overlay row0">
+    <div id="topbar" class="hoc clear">
+      <div class="fl_left"> 
+        <ul class="nospace"><img src="res\img\logo.png" alt=""></ul>
+      </div>
+    <div class="fl_right"> 
+       <ul class="nospace">
+        <li><a href="index.php"><i class="fas fa-home"></i></a></li>
+        <li><a href="#" title="Help Centre"><i class="far fa-life-ring"></i></a></li>
+        <li><a href="login.php" title="Login"><i class="fas fa-sign-in-alt"></i></a></li>
+        <li><a href="sign-up.php" title="Sign Up"><i class="fas fa-edit"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!-- head logo End -->
+
+<!-- Navbar -->
+<div class="bgded" style="background-image:url('res/img/01.png');"> 
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear">
+      <div id="logo" class="fl_left"> 
+        <h1 class="logoname"><a href="index.html"><span>Home </span></a></h1>
+      </div>
+        <nav id="mainav" class="fl_right"> 
+          <ul class="clear">
+           <li class="active"><a href="index.php">Home</a></li>
+           <li><a class="drop" href="#">Pages</a>
+           <li><a href="about.php">about</a></li>
+           <li><a href="login.php">Log In</a></li>
+           <li><a href="sign-up.php">Signup</a></li>
+          </ul>
+        </nav>
+    </header>
+<!-- Navbar End -->
+
+</head>
+
+<body id="top">
+
+   </div>
+    <div id="pageintro" class="hoc clear"> 
+      <article>
+        <p>Fresh Farm Products</p>
+       <h3 class="heading">Best quality for the best price</h3>
+        <footer>
+          <ul class="nospace inline pushright">
+             <li><a class="btn" href="about.php">Know Us More</a></li>
+             <li><a class="btn inverse" href="sign-up.php">Join Us</a></li>
+          </ul>
+        </footer>
+      </article>
+    </div>  
+  </div>
+
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+      <section id="introblocks" class="row3 inspace-30 clear">
+        <div class="one_half first">
+          <h6 class="heading">Mus sodales nulla integer</h6>
+          <p class="heading font-x3">Auctor diam sed non sapien in risus rhoncus egestas mauris rhoncus mi at odio in luctus turpis quis dignissim.</p>
+        </div>
+      <div class="one_half">
+        <ul class="nospace group">
+          <li class="one_half first btmspace-30">
+            <h6 class="heading">Interdum</h6>
+            <p>Placerat mi ipsum ornare dui id varius tortor est et urna vivamus commodo justo quis erat etiam lacus leo interdum non volutpat.</p>
+          </li>
+          <li class="one_half btmspace-30">
+            <h6 class="heading">Ullamcorper</h6>
+            <p>In sagittis vel erat donec congue pede eu lacus aliquam sodales felis nec libero mollis porttitor nulla adipiscing orci ac quam.</p>
+          </li>
+          <li class="one_half first">
+            <h6 class="heading">Accumsan</h6>
+            <p>Suspendisse elit praesent hendrerit lacus a condimentum scelerisque augue mi ultrices nunc vitae posuere orci dui quisque.</p>
+          </li>
+          <li class="one_half">
+            <h6 class="heading">Maecenas</h6>
+            <p>Tincidunt lectus vel enim aenean erat orci mollis quis gravida sed mollis a quam morbi libero sed ut erat aliquam erat volutpat.</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+   
+</main>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="bgded overlay row4" style="background-image:url('res/images/demo/backgrounds/01.png');">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="group">
+      <div class="one_quarter first">
+        <h1 class="logoname"><a href="index.html">Farm<span> Production </span>Auction</a></h1>
+        <p>Connect with us in our Social Media Accounts [<a href="#">&hellip;</a>]</p>
+        <ul class="faico clear">
+          <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+          <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a class="faicon-vk" href="#"><i class="fab fa-vk"></i></a></li>
+        </ul>
+      </div>
+      <div class="one_quarter">
+        <h6 class="heading">Register for Newsletter</h6>
+        <p class="nospace btmspace-15">For more information and Offers kindly register on our Newsletter</p>
+        <form action="#" method="post">
+          <fieldset>
+            <legend>Newsletter:</legend>
+            <input class="btmspace-15" type="text" value="" placeholder="Name">
+            <input class="btmspace-15" type="text" value="" placeholder="Email">
+            <button class="btn" type="submit" value="submit">Submit</button>
+          </fieldset>
+        </form>
+      </div>
+      <div class="one_quarter">
+        <h6 class="heading">Related Links</h6>
+        <ul class="nospace linklist">
+          <li><a href="#">Eu odio sit amet odio euismod</a></li>
+          <li><a href="#">Iaculis vestibulum eu ipsum</a></li>
+          <li><a href="#">Aliquam faucibus sed vehicula</a></li>
+          <li><a href="#">Congue diam fusce cursus</a></li>
+          <li><a href="#">Velit sed interdum consequat</a></li>
+        </ul>
+      </div>
+      <div class="one_quarter">
+        <h6 class="heading">People Behind this project</h6>
+        <ul class="nospace clear latestimg">
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+          <li><a class="imgover" href="#"><img src="res/images/demo/100x100.png" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- ################################################################################################ -->
+    <figure id="ctdetails">
+      <ul class="nospace group">
+        <li class="one_third first">
+          <div class="clear"><a href="#"><i class="fas fa-phone"></i></a> <span><strong>Give us a call:</strong> +00 (123) 456 7890</span></div>
+        </li>
+        <li class="one_third">
+          <div class="clear"><a href="#"><i class="fas fa-envelope"></i></a> <span><strong>Send us a mail:</strong> support@domain.com</span></div>
+        </li>
+        <li class="one_third">
+          <div class="clear"><a href="#"><i class="fas fa-map-marker-alt"></i></a> <span><strong>Come visit us:</strong> Directions to <a href="#">our location</a></span></div>
+        </li>
+      </ul>
+    </figure>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">JAGG</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
